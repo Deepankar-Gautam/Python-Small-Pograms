1f38130a78c83617f790efe88f389009bfeeba4b
Deepankar <deepankar.gautam1012@gmail.com>
2025-12-22 13:58:06 +0530

    updated files

--- Even and Odd.py
+++ Even and Odd.py
@@ -1,5 +1,5 @@
 #Even Odd
 
-# number = int (input ("Write a number of your choice\n"))
+number = int (input ("Write a number of your choice\n"))
 
-# print (f"The number {number} which you have given is even") if number % 2 == 0 else print (f"The number {number} which you have given is odd")
+print (f"The number {number} which you have given is even") if number % 2 == 0 else print (f"The number {number} which you have given is odd")
